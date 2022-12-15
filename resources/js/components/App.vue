0fb4da8d8b17bf7148e2ccb27ec8529f612c1a08
BlackSemkaJ <nekrashevich.jobs@gmail.com>
2022-12-15 16:50:05 +0300

    header

---
+++ resources/js/components/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <Header></Header> 
+    </div>
+</template>
+
+<script>
+    import Header from '../components/Header.vue'
+    export default {
+        name: "app",
+        components: {
+            Header,
+        }
+    }
+</script>
